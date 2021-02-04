8f5d33bd98f30b830806b9dbdd54640bba903d11
Victoria Wika <victoriawika@icloud.com>
2021-02-04 15:07:32 +0100

    Add Card component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import Button from './components/Button'
 import Header from './components/Header'
+import Card from './components/Card'
 
 const header = Header('Quiz App', 'May the best win!')
 document.body.append(header)
+
+const card = Card('What does git log do?', 'Show the history of commits')
+document.body.append(card)
 
 const button = Button('Click me!')
 
